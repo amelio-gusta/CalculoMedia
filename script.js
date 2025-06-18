cd31549667a292773e85ff6c26b056f3196a3607
Gusta <gustavo.amelio@aluno.senai.br>
2025-06-18 12:20:29 -0300

    Incluindo nota 3

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ function calcularMedia() {
     //Pega os valores dos campos
     var n1 = parseFloat(document.getElementById('nota1').value);
     var n2 = parseFloat(document.getElementById('nota2').value);
+    var n3 = parseFloat(document.getElementById('nota3').value);
 
     //Verifica se saõ números válidos
-    if (isNaN(n1) || isNaN(n2)) {
+    if (isNaN(n1) || isNaN(n2) || isNaN(3)) {
         alert("Por favor, insira notas válidas");
         return;
 
     }
 
     //calcula a média 
-    var media = (n1 + n2) / 2;
+    var media = (n1 + n2 + n3) / 3;
 
     //Mostra o resultado
-    document.getElementById('resultado').innerText = "A média é: " + media.toFixed(2);
+    document.getElementById('resultado').innerText = "A média é: " + media.toFixed(3);
 }
